a00d022c6bf52b598885527eb551f0ca5e103988
vickysoft1990 <66216342+vickysoft1990@users.noreply.github.com>
2021-06-05 18:12:17 +0530

    Update statistics.php

--- statistics.php
+++ statistics.php
@@ -9,7 +9,11 @@
    if(sizeof($statisticArr)<=0){
        echo"<p class='error'>No data present for the given date range, So the last 5 records are displayed!!</p>";
        $statisticArr = $Cobj->findSd($compList," order by sDate desc limit 0,5 ",")l order by l.sDate");
+       if(sizeof($statisticArr)<=0){
+           echo "Company ".$compList." Not present inside the Database ".$compList="Error";
+       }
    }
+if($compList!='Error'){
    $whr='';
    foreach($statisticArr as $val){
    $sDate=$val['sDate'];
@@ -20,16 +24,91 @@
    $trimedVal=rtrim($whr,',');
    $dynamicChart="data.addRows([
                   $trimedVal
-               ]);
-               ";
+                   ]); ";
                
-               
+    function calAnalysis($date1,$date2){
+        $earlier = new DateTime($date1);
+        $later = new DateTime($date2);
+        $diff = $later->diff($earlier)->format("%a");
+        return $diff;
+    }           
    
    ?>
 <?php include 'header.php';?>
 <body>
    <div class="container1"     style="padding-right: 15px; padding-left: 15px; margin-right: auto; "
       >
+       <div class="row">
+         <div class="col-sm-5" style = "margin: 70px auto">
+            <table class="table table-bordered">
+               <thead>
+                  <p>
+                     <?php 
+                        if($flag!=""){ 
+                        echo "<p class='error'>Already one transcation made, Thats all for today!!</p>";
+                        
+                        } 
+                        ?>
+                  </p>
+                  <tr>
+                     <th>Type</th>
+                     <th>Date</th>
+                     <th>Days</th>
+                     <th>Profit</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <?php
+                  $startTimeStamp="";
+                  $mode="Buy";
+                 $size=sizeof($statisticArr);
+                 for($i=0;$i<$size;$i++){
+                         $dataPoints[$i]['y']=$statisticArr[$i]['sPrice'];
+                         $dataPoints[$i]['label']=$statisticArr[$i]['sDate'];
+                         $pro_loss=$statisticArr[$i]['profit_or_loss'];
+                        if($pro_loss<0 && $mode!='Buy'){
+                        $date2=$statisticArr[$i]['sDate'];
+                        $days=calAnalysis($date1,$date2);
+                        echo "<tr><td>Sell on :(Evening-Trading Close)</td><td>".$statisticArr[$i]['sDate']."</td><td>".$days." </td><td align='right'> ".$totPoss."</td></tr>";
+                         $mode="Buy";
+                        $totDays=$totDays+$days;
+                        }
+                        elseif($pro_loss>0 && $mode!='Sell'){
+                        $date1=$statisticArr[$i]['sDate'];
+                        echo "<tr><td>Buy on :(Morning-Trading starts)</td><td>".$statisticArr[$i]['sDate']."</td><td></td><td></td></tr>";
+                         $mode="Sell";
+                        }
+                        if($pro_loss>0){
+                     $totPoss=$totPoss+$pro_loss;
+                     $gt=$gt+$totPoss;
+                        }else{
+                            $totPoss=0;
+                        }
+                        
+                        if($size-1==$i && $mode=='Sell'){
+                         echo $mode."Profit Will be :".$totPoss;
+                     }
+                     }
+                                      echo"<tr><td></td><td>Total :</td><td>".$totDays."</td><td align='right'>".$gt."</td></tr>";
+
+                   /*  $i=0;
+                     foreach($statisticArr as $dataVal){
+                         $dataPoints[$i]['y']=$dataVal['sPrice'];
+                         $dataPoints[$i]['label']=$date1=$dataVal['sDate'];
+                        echo $date2=next($dataVal['sDate']);
+                         echo calDate($date1,$date2);
+                        $i++;
+                     }
+                     */
+                     ?>
+                  </tbody>
+            </table>
+         </div>
+         <div class="col-sm-4" id="chartContainer" style = "width: 550px; height: 400px; margin: 0 auto">
+            <div class="well">
+            </div>
+         </div>
+      </div>
       <div class="row">
          <div class="col-sm-5" style = "margin: 70px auto">
             <table class="table table-bordered">
@@ -52,7 +131,7 @@
                <tbody>
                   <?php
                      for($i=0;$i<sizeof($statisticArr);$i++){
-                         $arrval[]=$statisticArr[$i]['sPrice'];
+                     $arrval[]=$statisticArr[$i]['sPrice'];
                      echo"<tr>";
                      echo" <td>".$statisticArr[$i]['sDate']."</td>";
                      echo" <td>".$statisticArr[$i]['sPrice']."</td>";
@@ -103,6 +182,23 @@
 </body>
 <script>
    $(document).ready(function(){
+    var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Price Analysis"
+	},
+	axisY: {
+		title: "Stock Price"
+	},
+	data: [{
+		type: "column",
+		yValueFormatString: "#,##0.## Rupees",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+    });
+    chart.render();
+ 
    
     $('.userinfo').click(function(){
       
@@ -147,4 +243,14 @@
     google.charts.setOnLoadCallback(drawChart);
    	
 </script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
 </html>
+
+<?php
+}
+else{
+echo "Data Not found!! Go back and find new search!!";
+}
+?>
+
